a8b14c6754d3ffa7c97774e92ebecc96b09fa77a
Jackson Delahunt <jackson@stemn.com>
2019-11-18 12:03:06 +1100

    initial-commit

---
+++ .config/dnsmasq/update-config.sh
@@ -0,0 +1,2 @@
+wget -O ~/.config/dnsmasq/hostnames.txt -qnv https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
+wget -O ~/.config/dnsmasq/domains.txt -qnv https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
